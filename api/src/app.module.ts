2350c4c03d6e97a45ce405feb382b43ac7029f82
Oleksandr Ivanyshyn <ivanishin.alexander@lll.kpi.ua>
2026-02-01 18:06:26 +0200

    feat(api): create Card module resources with boilerplate code

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { BoardModule } from './board/board.module';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot(), BoardModule],
+  imports: [PrismaModule, ConfigModule.forRoot(), BoardModule, CardModule],
 })
 export class AppModule {}
